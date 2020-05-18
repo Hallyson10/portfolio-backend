267065a999bd59dc571a5cf6779c2b3b1f96b626
Hallyson Miranda <hallysonjean@hotmail.com>
2020-05-18 14:33:32 -0300

    teste cors

--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ require('./database');
 const cors = require('cors')
 
 // app.use(function(req, res, next) {
-//     res.header("Access-Control-Allow-Origin", process.env.URL_API); // update to match the domain you will make the request from
+//     res.header("Access-Control-Allow-Origin", ''); // update to match the domain you will make the request from
 //     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 //     next();
 //   });
