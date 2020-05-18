93b6d32bf12e5f61efe44e5dd277a8376756a23d
Hallyson Miranda <hallysonjean@hotmail.com>
2020-05-18 13:54:55 -0300

    testando tirando restriçoes de acesso

--- src/server.js
+++ src/server.js
@@ -6,11 +6,11 @@ const app = express();
 require('./database');
 const cors = require('cors')
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", process.env.URL_API); // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
+// app.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", process.env.URL_API); // update to match the domain you will make the request from
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+//   });
 app.use(express.json());
 app.use(cors())
 app.use(routes);
